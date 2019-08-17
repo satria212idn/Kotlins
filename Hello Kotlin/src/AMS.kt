3a51db9dcbdaf3c722f5832be2d16a6794a17a9a
Alfian Satria Vivaldi Payu <masfian77@gmail.com>
2019-08-17 22:04:01 +0700

    Add files via upload

---
+++ Hello Kotlin/src/AMS.kt
@@ -0,0 +1,32 @@
+import java.util.*
+
+fun main(args: Array<String> ) {
+    println("Hello, ${args[0]}!")
+    feedTheFish()
+}
+
+fun feedTheFish() {
+    val day = randomDay()
+    val food = fishFood(day)
+    println("Today is $day, and the Fish eat $food.")
+}
+
+fun randomDay() : String {
+    val week = listOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+    return week[Random().nextInt(7)]
+}
+
+fun fishFood (day: String) : String {
+    var food = "fasting"
+
+    return when (day) {
+        "Monday" -> "flakes"
+        "Tuesday" -> "pellets"
+        "Wednesday" -> "redworms"
+        "Thursday" -> "granules"
+        "Friday" -> "mosquitos"
+        "Saturday" -> "lettuce"
+        "Sunday" -> "plankton"
+        else -> "fasting"
+    }
+}
