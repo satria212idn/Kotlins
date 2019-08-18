93f6398ce2fe9e70247839f510b67e52c55c2a07
Alfian Satria Vivaldi Payu <masfian77@gmail.com>
2019-08-19 01:07:29 +0700

    Add files via upload

---
+++ AquariumTwo/src/Aquarium5/Aquarium5.kt
@@ -0,0 +1,31 @@
+package Aquarium5
+
+data class Fish (var name: String)
+
+fun main(args: Array<String>) {
+    fishExample()
+}
+
+fun fishExample() {
+    val fish = Fish("splashy")
+
+    myWith (fish.name){
+        println(capitalize())
+    }
+
+    println(fish.run {name})
+
+    println(fish.apply {})
+
+    val fish2 = Fish(name = "splashy").apply {name= "Sharky"}
+    println(fish2.name)
+
+    println (fish.let {it.name.capitalize()}
+        .let {it + "fish"}
+        .let {it.length}
+        .let {it + 31})
+}
+
+fun myWith(name: String, block: String.() -> Unit) {
+    name.block()
+}
